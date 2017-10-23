b149af2294e5c194ec7df60aae26c7d41e5a1e17
Yuriy Senko <yuriy.senko@essencedigital.com>
2017-10-23 11:48:37 +0300

    [OTD-8088] Pin dependency versions to prevent deadlocks in Olive3.

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from distutils.core import setup
 
 setup(
     name='Google MRF',
-    version='0.1',
+    version='0.2',
     description='Client for Google MRF and media measurement schema service.',
     author='Josh Fyne',
     author_email='josh.fyne@essencedigital.com',
     url='https://github.com/essence-tech/google-mrf-interface',
     packages=['googlemrf'],
-    install_requires=['grpcio', 'protobuf'],
+    install_requires=[
+        'grpcio==1.4.0',
+        'protobuf==3.4.0'
+    ],
     )
